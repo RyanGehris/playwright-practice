00da2ec773d14b80fabfd93c856609ecb79541d0
Ryan Gehris <rogehris@gmail.com>
2023-04-13 19:02:46 -0700

    created test to check login

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,21 +1,27 @@
 // @ts-check
+require('dotenv').config()
 const { test, expect } = require('@playwright/test');
 
-test('create slide show', async ({ page }) => {
+test('Login to Canva', async ({ page }) => {
   // ARRANGE
   // go to canva
   await page.goto('https://www.canva.com/');
   // Add a random delay of 1 to 5 seconds to simulate human behavior
   await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 4000 + 1000)));
-  // login
-  await page.click(':text("Log in")');
-  // Add a random delay of 1 to 5 seconds to simulate human behavior
-  await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 4000 + 1000)));
-  await page.click(':text("Continue with email")');
-
-  // add photo 1
-  // add photo 2
 
   // ACT
+  // click log in button
+  await page.click(':text("Log in")');
+  // click continue with email option
+  await page.click(':text("Continue with email")');
+  // input email and continue
+  await page.locator('[name="email"]').fill(process.env.EMAIL)
+  await page.click(':nth-match(:text("Continue"), 2)');
+  // input password and log in
+  await page.locator('[name="password"]').fill(process.env.PASSWORD)
+  await page.click(':nth-match(:text("Log in"), 2)');
+
   // ASSERT
+  // check if "Create a design is visibile"
+  await expect(page.locator('text:("Create a design")')).toBeVisible()
 });
